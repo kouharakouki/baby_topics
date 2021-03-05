14e22c498fe6ffbd8571a4b701b16dec13847348
kouharakouki <koki.kohara916@gmail.com>
2021-03-05 15:02:01 +0000

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+  devise_for :users
+  get 'users/:id/unsubscribe' => 'users#unsubscribe', as: 'users_unsubscribe'
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 end
